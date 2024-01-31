13b71e7f4cb159eb74e974c12c0feb94851c35c5
GiuseppeBuonasera <121633690+GiuseppeBuonasera@users.noreply.github.com>
2024-01-31 11:00:19 +0100

    Update cone_recorder.py

--- cone_recorder.py
+++ cone_recorder.py
@@ -1,25 +1,30 @@
 import rospy
 import math
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import PoseStamped , Point
 from sensor_msgs.msg import PointCloud2 
 import sensor_msgs.point_cloud2 as pc2
+import threading
+import time
 import std_msgs.msg
-import numpy as np
-from visualization_msgs.msg import Marker
+from visualization_msgs.msg import Marker, MarkerArray
 
 errore = 0.5
 count=0
 conteggio_punti = {} 
 punti_filtrati = []
 buffer = []
+
 # modificare il codice in modo tale da guardare per prima la lista definitiva
+
 
 
 def callback(data):
     global count
     punti_circo = []
+    
     x = data.x
     y = data.y
+    
     point = x, y
     #gestisco i punti nulli
     if point == (0,0):
@@ -27,31 +32,38 @@ def callback(data):
     else:
         buffer.append(point)
     
+    
     for valori in buffer: 
+           
         filtra_punti(valori, errore)
 
     #quando il counter va a 20 pulisco le liste     
-    if count == 20:   
+    if count == 30:   
         conteggio_punti.clear()
+        punti_circo.clear()
         buffer.clear()
+
         count = 0
     else:
         count +=1
     
    
     #pubblicazione cerchi su rviz
-    for a in punti_filtrati:
-        punti_cerchi = circonferenza(a, r = 0.5, numero_punti=360)
-        punti_circo.append(punti_cerchi)
-        publish_circle(punti_circo)
-    spawn_cone(punti_filtrati)
+    #for a in punti_filtrati:
+        #punti_cerchi = circonferenza(a, r=0.5, numero_punti=360)
+        #punti_circo.append(punti_cerchi)
+    publish_cylinders(punti_filtrati)
+
     print(punti_filtrati)
-    punti_circo.clear()
+    
+    
+
 
 
 
 def lista_conteggio_punti(punti):
     x,y = punti
+    
     trovato = False
     for (x1, y1), conteggio in conteggio_punti.items():
         differenza_x = abs(x - x1)
@@ -74,7 +86,7 @@ def lista_conteggio_punti(punti):
             
     return punti_filtrati
  
-
+#media posizioni
 
 def filtra_punti(points, errore):
     x1, y1 = points
@@ -96,7 +108,7 @@ def filtra_punti(points, errore):
 
 def publish_circle(punti_circo):
 
-    pub1 = rospy.Publisher('/circle_topic', PointCloud2, queue_size=100)
+    pub1 = rospy.Publisher('/circle_topic', PointCloud2, queue_size=10)
     header1 = std_msgs.msg.Header()
     header1.stamp = rospy.Time.now()
     header1.frame_id = "odom"   
@@ -105,7 +117,7 @@ def publish_circle(punti_circo):
         points2 = [(xc,yc,0.0) for xc,yc in e]
         cloud_msg2 = pc2.create_cloud_xyz32(header1, points2)
 
-    rate1 = rospy.Rate(2000) 
+    rate1 = rospy.Rate(1) 
     rate1.sleep()
     pub1.publish(cloud_msg2)
     points2.clear()
@@ -126,22 +138,33 @@ def circonferenza(centro, r, numero_punti=360):
     return punti_circonferenza
 
 
-def spawn_cone(punti_filtrati):
-    for (x,y) in punti_filtrati:
-        cono = Marker()
-        cono.type = Marker.CUBE
-        cono.pose.position = Point(x,y, 0.0) #punti cono
-        cono.scale.x = 1.0 #dimensioni 
-        cono.scale.y = 1.0
-        cono.scale.z = 2.0
-        cono.color.a = 1.0
-        cono.color.r = 1.0
-        cono.color.g = 0.0
-        cono.color.b = 0.0
+def publish_cylinders(points):
+    
+    
+    marker_publisher = rospy.Publisher('/cylinder', MarkerArray, queue_size=10)
+    rate = rospy.Rate(1) 
+    cylinderarray = MarkerArray()
+    for i,point in enumerate(points):
+        
+        cylinder = Marker()
+        cylinder.id = i
+        cylinder.header.frame_id = "base_link"
+        cylinder.type = Marker.CYLINDER
+        cylinder.pose.position = Point(point[0], point[1], 0.5)  
+        cylinder.pose.orientation.w = 1.0 
+        cylinder.scale.x = 1  # Diametro del cilindro
+        cylinder.scale.y = 1  # Diametro del cilindro
+        cylinder.scale.z = 1.0  # Altezza del cilindro
+        cylinder.color.a = 1.0
+        cylinder.color.r = 1.0
+        cylinder.color.g = 0.0
+        cylinder.color.b = 0.0
+        cylinderarray.markers.append(cylinder)
+    marker_publisher.publish(cylinderarray)
+    
 
-        cono_publisher = rospy.Publisher('/visualization_cone', Marker, queue_size=10)
-        cono_publisher.publish(cono)
-        rospy.sleep(1)
+        
+        
 
 
 def main ():
